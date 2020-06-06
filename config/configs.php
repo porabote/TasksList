50513a4918b279a74af5a80658065cf406e8f15c
Porabote <git@porabote.com>
2020-06-06 20:05:32 +0300

    test

--- config/configs.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('ROOT', dirname(__DIR__));
-
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'apiAdmin');
-define('DB_PASSWD', 'apiAdmin7893727');
-define('DB_NAME', 'tmp_tasks');
-?>
